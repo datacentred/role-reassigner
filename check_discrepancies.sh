1ee69560487fd251eda773669a820a72b71e4adb
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2016-07-15 10:22:21 +0100

    Don't look for names.
    Previous version of the script was translating UUIDs to names;
    the way it was done is very inefficient. This commit removes
    the translation bit which will be moved to a separate script
    so it can be executed on the results from this version.

--- check_discrepancies.sh
+++ check_discrepancies.sh
@@ -4,7 +4,8 @@
 
 if [[ $# -ne 1 ]] 
 then
-    echo "usage: $0 <stronghold_input>"; exit 1
+    echo "usage: $0 <stronghold_input>"
+    exit 1
 fi
 
 INPUT_STRONGHOLD=$1
@@ -14,6 +15,7 @@ OPENSTACK=`which openstack`
 OUTPUT_DISC="./output_discrepancies.csv"
 test -f $OUTPUT_DISC && rm $OUTPUT_DISC
 
+# go thorought the whole reference file (csv) and parse columns
 OLDIFS=$IFS
 IFS=","
 while read user_id project_id role_id
@@ -22,11 +24,8 @@ do
     PROJECT : $project_id\n \
     ROLE : $role_id\n"
 
-    eval `$OPENSTACK user show $user_id -f shell --prefix openstack_ | grep ^openstack_username`
-    eval `$OPENSTACK project show $project_id -f shell --prefix openstack_project_ | grep ^openstack_project_name`
-    eval `$OPENSTACK role show $role_id -f shell --prefix openstack_role_ | grep ^openstack_role_name`
-
-    test -z `$OPENSTACK user role list $user_id --project $project_id | grep $role_id` && echo "$user_id,$project_id,$role_id,$openstack_username,$openstack_project_name,$openstack_role_name" >> $OUTPUT_DISC
+    # check which roles are not present
+    test -z `$OPENSTACK user role list $user_id --project $project_id | grep $role_id` && echo "$user_id,$project_id,$role_id" >> $OUTPUT_DISC
 done < $INPUT_STRONGHOLD
 IFS=$OLDIFS
 
